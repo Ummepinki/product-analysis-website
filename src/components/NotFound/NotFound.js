fe92c611cd34f5609b301d7c4db2b5a9a38f5981
Ummepinki <salmaumme888@gmail.com>
2022-04-05 23:18:52 +0600

    readme file added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>This is not found page</h1>
-            <p>404</p>
+            <h1 className='mt-4'>This is not found page</h1>
+            <p className='font-bold'>404</p>
         </div>
     );
 };
